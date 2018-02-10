a792a6e0410349249e669e074b0c0e86b2a67c9b
Andrew Oh <andrewoh531@gmail.com>
2018-02-11 08:28:46 +1100

    Change font to black

--- lib/image_manipulator.go
+++ lib/image_manipulator.go
@@ -13,7 +13,7 @@ func AddTextToImage(img *image.NRGBA, text string) {
 }
 
 func addLabelHelper(img *image.NRGBA, x, y int, label string) {
-	col := color.RGBA{200, 100, 0, 255}
+	col := color.RGBA{0, 0, 0, 255}
 	point := fixed.Point26_6{fixed.Int26_6(x * 64), fixed.Int26_6(y * 64)}
 
 	d := &font.Drawer{
